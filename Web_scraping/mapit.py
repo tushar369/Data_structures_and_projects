7289a7b98ee6edd928c1aef6ec2e5d1cd69250f6
Jay <tusharyadav34@gmail.com>
2020-12-28 11:53:13 +0530

    web_scraping

--- Web_scraping/mapit.py
+++ Web_scraping/mapit.py
@@ -2,11 +2,13 @@ import webbrowser
 import sys
 import pyperclip
 
+
 if len(sys.argv) > 1:
     address = ' '.join(sys.argv[1:])
 else:
     address = pyperclip.paste()
 
 webbrowser.open('https://www.google.co.in/maps/place/' + address)
+
 
 
---
+++ Web_scraping/requests_practice.py
@@ -0,0 +1,7 @@
+import requests
+
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+print(res.raise_for_status())
+print(type(res))
+print(res.status_code == 200)
+print(res.text[:250])
