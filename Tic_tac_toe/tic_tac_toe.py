44b2a2b3af6b653e4dac916c4867a2578dfb8b98
Jay <tusharyadav34@gmail.com>
2020-12-29 22:21:46 +0530

    tic_tac_toe-update

--- Tic_tac_toe/tic_tac_toe.py
+++ Tic_tac_toe/tic_tac_toe.py
@@ -1,11 +1,10 @@
 empty_board = [['-', '-', '-'],
                ['-', '-', '-'],
                ['-', '-', '-']]
-board_is_empty = False
+board_is_empty = True
 turn = 'X'
 
 
-#display board
 def display_board(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
@@ -16,7 +15,7 @@ def display_board(board):
 
 
 def check_board(board, pos):
-
+    #It checks the winner
     #check row
     pos_row = board[pos[0]]
     if ['X', 'X', 'X'] == pos_row or ['O', 'O', 'O'] == pos_row:
@@ -30,21 +29,26 @@ def check_board(board, pos):
     pos_col.append(pos)        
     if ['X', 'X', 'X'] == pos_col or ['O', 'O', 'O'] == pos_col:
         return True
-    
+
     #check diagonal
     diagnol_positions = [(0, 0), (1, 1), (2, 2), (2, 0), (0, 2)] 
     diagnol_1 = [board[0][0], board[1][1], board[2][2]]
     diagnol_2 = [board[2][0], board[1][1], board[0][2]]
+
     if pos in diagnol_positions:
         if ['X', 'X', 'X'] == diagnol_1 or ['O', 'O', 'O'] == diagnol_1:
             return True
     if pos in diagnol_2:
         if ['X', 'X', 'X'] == diagnol_2 or ['O', 'O', 'O'] == diagnol_2:
-            return True  
+            return True 
 
-    return None          
+    return None         
 
+def check_tie(board):
+    if '-' not in board[0] and '-' not in board[1] and '-' not in board[2]:
+        return True        
 
+# choosing the position
 def move():
     move_row = input(f'Choose a row(1, 2, 3) player {turn}: ')
     move_row = int(move_row) - 1
@@ -76,17 +80,24 @@ def play_game(board):
         board_is_empty = False
     else:
         turn = flip_turn(turn)
+
     while True:    
         row, col = move()
         if move_validation(board, (row, col)):
             break
+        
     board[row][col] = turn
+
+    if check_tie(board):
+        print("It's a tie")  
+        return True 
+
     if check_board(board, (row, col)):
         display_board(board)
         print(f"{turn} won !!")
         return True
     else:
-        play_game(board)    
+        play_game(board) 
 
         
 play_game(empty_board)    
